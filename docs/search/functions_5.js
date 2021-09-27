be04e34018d7a00ca2cf980ecdbf13a110c6eb37
acd1034 <48613285+acd1034@users.noreply.github.com>
2021-09-27 06:52:08 +0000

    Deploying to gh-pages from  @ b5fbeedd6055fa142ad70dcf1e15a16b8117705c 🚀

--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['imag_178',['imag',['../math_8hpp.html#aa8922ff061ea3b82a7e31837fdb648bd',1,'kspc']]],
-  ['innerp_179',['innerp',['../math_8hpp.html#a3332d85228da007f0095334443521c3a',1,'kspc::innerp(R1 &amp;&amp;r1, R2 &amp;&amp;r2, P1 proj1={}, P2 proj2={}, BOp1 bop1={}, BOp2 bop2={})'],['../math_8hpp.html#a22bd87c4971ffd49e68e3119dc4533c4',1,'kspc::innerp(R1 &amp;&amp;r1, R2 &amp;&amp;r2, R3 &amp;&amp;r3, P1 proj1={}, P2 proj2={}, P3 proj3={}, BOp1 bop1={}, BOp2 bop2={}, BOp3 bop3={})']]],
+  ['innerp_179',['innerp',['../math_8hpp.html#a3332d85228da007f0095334443521c3a',1,'kspc::innerp(R1 &amp;&amp;r1, R2 &amp;&amp;r2, P1 proj1={}, P2 proj2={}, BOp1 bop1={}, BOp2 bop2={})'],['../math_8hpp.html#a4191c40428cc9ba061996fa0d7ce596b',1,'kspc::innerp(R1 &amp;&amp;r1, R2 &amp;&amp;r2, R3 &amp;&amp;r3, P1 proj1={}, P2 proj2={}, P3 proj3={}, BOp1 bop1={}, BOp2 bop2={}, BOp3 bop3={})']]],
   ['isqrt_180',['isqrt',['../math_8hpp.html#a65d1cf5a3f68cba8c253183d1cb21d98',1,'kspc']]]
 ];
