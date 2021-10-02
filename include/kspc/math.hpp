38a4887a80a585725ddb52d0da89b030787b0b80
acd1034 <48613285+acd1034@users.noreply.github.com>
2021-10-02 15:10:58 +0900

    #1 Move functions
    List of moved functions:
    - operator+,-,*,/ for std::array
    - operator+,-,*,/ for std::vector

--- include/kspc/math.hpp
+++ include/kspc/math.hpp
@@ -171,6 +171,119 @@ namespace kspc {
     inline constexpr auto dim = detail2::dim_fn{};
   } // namespace cpo
 
+  // arithmetic operator for `std::array`, `std::vector`
+
+  inline namespace arithmetic_ops {
+    // array
+    template <typename T, std::size_t N,
+              std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+    inline constexpr auto& operator+(const std::array<T, N>& x) {
+      return x;
+    }
+
+    template <typename T, std::size_t N,
+              std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+    inline constexpr auto operator-(const std::array<T, N>& x) {
+      std::array<T, N> ret{};
+      for (std::size_t i = 0; i < N; ++i) ret[i] = -x[i];
+      return ret;
+    }
+
+    template <typename T, std::size_t N,
+              std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+    inline constexpr auto operator+(const std::array<T, N>& x, const std::array<T, N>& y) {
+      std::array<T, N> ret{};
+      for (std::size_t i = 0; i < N; ++i) ret[i] = x[i] + y[i];
+      return ret;
+    }
+
+    template <typename T, std::size_t N,
+              std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+    inline constexpr auto operator-(const std::array<T, N>& x, const std::array<T, N>& y) {
+      std::array<T, N> ret{};
+      for (std::size_t i = 0; i < N; ++i) ret[i] = x[i] - y[i];
+      return ret;
+    }
+
+    template <typename T, std::size_t N,
+              std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+    inline constexpr auto operator*(T val, const std::array<T, N>& x) {
+      std::array<T, N> ret{};
+      for (std::size_t i = 0; i < N; ++i) ret[i] = val * x[i];
+      return ret;
+    }
+
+    template <typename T, std::size_t N,
+              std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+    inline constexpr auto operator*(const std::array<T, N>& x, T val) {
+      std::array<T, N> ret{};
+      for (std::size_t i = 0; i < N; ++i) ret[i] = x[i] * val;
+      return ret;
+    }
+
+    template <typename T, std::size_t N,
+              std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+    inline constexpr auto operator/(const std::array<T, N>& x, T val) {
+      std::array<T, N> ret{};
+      for (std::size_t i = 0; i < N; ++i) ret[i] = x[i] / val;
+      return ret;
+    }
+
+    // vector
+    template <typename T, std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+    inline constexpr auto& operator+(const std::vector<T>& x) {
+      return x;
+    }
+
+    template <typename T, std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+    inline constexpr auto operator-(const std::vector<T>& x) {
+      const auto n = std::size(x);
+      std::vector<T> ret(n);
+      for (std::size_t i = 0; i < n; ++i) ret[i] = -x[i];
+      return ret;
+    }
+
+    template <typename T, std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+    inline constexpr auto operator+(const std::vector<T>& x, const std::vector<T>& y) {
+      const auto n = std::min(std::size(x), std::size(y));
+      std::vector<T> ret(n);
+      for (std::size_t i = 0; i < n; ++i) ret[i] = x[i] + y[i];
+      return ret;
+    }
+
+    template <typename T, std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+    inline constexpr auto operator-(const std::vector<T>& x, const std::vector<T>& y) {
+      const auto n = std::min(std::size(x), std::size(y));
+      std::vector<T> ret(n);
+      for (std::size_t i = 0; i < n; ++i) ret[i] = x[i] - y[i];
+      return ret;
+    }
+
+    template <typename T, std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+    inline constexpr auto operator*(T val, const std::vector<T>& x) {
+      const auto n = std::size(x);
+      std::vector<T> ret(n);
+      for (std::size_t i = 0; i < n; ++i) ret[i] = val * x[i];
+      return ret;
+    }
+
+    template <typename T, std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+    inline constexpr auto operator*(const std::vector<T>& x, T val) {
+      const auto n = std::size(x);
+      std::vector<T> ret(n);
+      for (std::size_t i = 0; i < n; ++i) ret[i] = x[i] * val;
+      return ret;
+    }
+
+    template <typename T, std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+    inline constexpr auto operator/(const std::vector<T>& x, T val) {
+      const auto n = std::size(x);
+      std::vector<T> ret(n);
+      for (std::size_t i = 0; i < n; ++i) ret[i] = x[i] / val;
+      return ret;
+    }
+  } // namespace arithmetic_ops
+
   // sum
 
   // clang-format off
