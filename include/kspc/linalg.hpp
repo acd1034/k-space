4d643989e731a9e1f82dae6fe3f45c5e9e2fc5e5
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-01-06 17:46:44 +0900

    New `linalg.hpp`

--- include/kspc/linalg.hpp
+++ include/kspc/linalg.hpp
@@ -1,105 +1,237 @@
-/// @file linalg.hpp
-#pragma once
 #include <array>
-#include <complex>
 #include <vector>
 #include <kspc/math_basics.hpp>
 
+// clang-format off
+
+// general matrix linear solve
 namespace kspc {
   /// @addtogroup linalg
   /// @{
 
   /// @cond
-  namespace lapack {
+  namespace detail {
     extern "C" {
-    void zheev_(const char& JOBZ, const char& UPLO, const int& N, std::complex<double>* A,
-                const int& LDA, double* W, std::complex<double>* WORK, const int& LWORK,
-                double* RWORK, int& INFO);
-    }
-  } // namespace lapack
-    /// @endcond
+      // double
 
-  /// @brief diagonalize hermitian matrix (fixed-size)
-  /// @example haldane_fixed.cpp
-  // clang-format off
-  template <typename L,
-            std::enable_if_t<std::conjunction_v<
-              is_sized_range<L>,
-              is_complex<std::decay_t<range_reference_t<L>>>,
-              is_fixed_size_array<L>>, std::nullptr_t> = nullptr>
-  // clang-format on
-  auto zheev(const L& H) {
-    using T = complex_value_t<std::decay_t<range_reference_t<L>>>;
-    constexpr auto N = isqrt(fixed_size_array_size_v<L>);
-    static std::complex<T> temp_H[N * N];
-    static T temp_E[N];
-    static std::complex<T> cwork[N * 4];
-    static T rwork[N * 4];
+      // LU factorization
+      // http://www.netlib.org/lapack/explore-html/dd/d9a/group__double_g_ecomputational_ga0019443faea08275ca60a734d0593e60.html#ga0019443faea08275ca60a734d0593e60
+      void dgetrf_(const std::size_t& m,
+                  const std::size_t& n,
+                  double* A, // M-by-N matrix
+                  const std::size_t& lda,
+                  std::size_t* ipiv,
+                  int& info);
+
+      // solve Ax = b with a general matrix A using LU factorization
+      // http://www.netlib.org/lapack/explore-html/dd/d9a/group__double_g_ecomputational_ga58e332cb1b8ab770270843221a48296d.html#ga58e332cb1b8ab770270843221a48296d
+      void dgetrs_(const char& trans,
+                  const std::size_t& n,
+                  const std::size_t& nrhs,
+                  const double* A, // N-by-N matrix
+                  const std::size_t& lda,
+                  const std::size_t* ipiv,
+                  double* b, // N-by-NRHS matrix
+                  const std::size_t& ldb,
+                  int& info);
+
+      // complex double
+
+      // LU factorization
+      void zgetrf_(const std::size_t& m,
+                  const std::size_t& n,
+                  std::complex<double>* A, // M-by-N matrix
+                  const std::size_t& lda,
+                  std::size_t* ipiv,
+                  int& info);
+
+      // solve Ax = b with a general matrix A using LU factorization
+      void zgetrs_(const char& trans,
+                  const std::size_t& n,
+                  const std::size_t& nrhs,
+                  const std::complex<double>* A, // N-by-N matrix
+                  const std::size_t& lda,
+                  const std::size_t* ipiv,
+                  std::complex<double>* b, // N-by-NRHS matrix
+                  const std::size_t& ldb,
+                  int& info);
+    }
+  }
+  /// @endcond
+
+  /// LU factorization
+  template <class InOutMat, class OutIPiv>
+  int lu_factor(InOutMat& A, OutIPiv& ipiv) {
+    using std::size, std::data;
+    const std::size_t n = kspc::dim(A);
+    // std::cout << n << std::endl;
+    assert(size(A) == n * n);
+    assert(size(ipiv) == n);
+
     int info;
-
-    for (std::size_t j = 0; j < N; ++j) {
-      for (std::size_t k = 0; k < N; ++k) {
-        temp_H[k * N + j] = H[j * N + k];
-      }
+    if constexpr (is_complex_v<range_value_t<InOutMat>>) {
+      //      zgetrf_(m, n,      A , lda,      ipiv , info)
+      detail::zgetrf_(n, n, data(A),   n, data(ipiv), info);
+    } else {
+      //      dgetrf_(m, n,      A , lda,      ipiv , info)
+      detail::dgetrf_(n, n, data(A),   n, data(ipiv), info);
     }
-
-    lapack::zheev_('V', 'L', N, temp_H, N, temp_E, cwork, N * 4, rwork, info);
-
-    std::array<T, N> E{};
-    std::array<std::array<std::complex<T>, N>, N> U{};
-    for (std::size_t j = 0; j < N; ++j) {
-      E[j] = temp_E[j];
-      for (std::size_t k = 0; k < N; ++k) {
-        // U[j] is an eigenvector of H with eigenvalue E[j]
-        U[j][k] = temp_H[j * N + k];
-      }
-    }
-
-    return std::make_pair(std::move(E), std::move(U));
+    return info;
   }
 
-  /// @brief diagonalize hermitian matrix (dynamic-size)
-  /// @example haldane.cpp
-  // clang-format off
-  template <typename M,
-            std::enable_if_t<std::conjunction_v<
-              is_sized_range<M>,
-              is_complex<std::decay_t<range_reference_t<M>>>,
-              std::negation<is_fixed_size_array<M>>>, std::nullptr_t> = nullptr>
-  // clang-format on
-  auto zheev(const M& H) {
-    using T = complex_value_t<std::decay_t<range_reference_t<M>>>;
-    const std::size_t N = kspc::dim(H);
-    auto* temp_H = new std::complex<T>[N * N];
-    auto* temp_E = new T[N];
-    auto* cwork = new std::complex<T>[N * 4];
-    auto* rwork = new T[N * 4];
+  /// solve Ax = b with a general matrix A using LU factorization
+  template <class InMat, class InIPiv, class InOutVec>
+  int matrix_vector_solve_with_lu_factor(const InMat& A, const InIPiv& ipiv, InOutVec& b) {
+    using std::size, std::data;
+    const std::size_t n = kspc::dim(A);
+    // std::cout << n << std::endl;
+    assert(size(A) == n * n);
+    assert(size(ipiv) == n);
+    assert(size(b) == n);
+
     int info;
-
-    for (std::size_t j = 0; j < N; ++j) {
-      for (std::size_t k = 0; k < N; ++k) {
-        temp_H[k * N + j] = H[j * N + k];
-      }
+    if constexpr (is_complex_v<range_value_t<InMat>>) {
+      //      zgetrs_(trans, n, nrhs,      A , lda,      ipiv ,      b , ldb, info)
+      detail::zgetrs_(  'N', n,    1, data(A),   n, data(ipiv), data(b),   n, info);
+    } else {
+      //      dgetrs_(trans, n, nrhs,      A , lda,      ipiv ,      b , ldb, info)
+      detail::dgetrs_(  'N', n,    1, data(A),   n, data(ipiv), data(b),   n, info);
     }
+    return info;
+  }
 
-    lapack::zheev_('V', 'L', N, temp_H, N, temp_E, cwork, N * 4, rwork, info);
+  /// solve Ax = b with a general matrix A
+  template <class InOutMat, class OutIPiv, class InOutVec>
+  int matrix_vector_solve(InOutMat& A, OutIPiv& ipiv, InOutVec& b) {
+    int info;
+    info = lu_factor(A, ipiv);
+    if (info) return info;
+    info = matrix_vector_solve_with_lu_factor(A, ipiv, b);
+    return info;
+  }
 
-    std::vector<T> E(N);
-    std::vector<std::vector<std::complex<T>>> U(N, std::vector<std::complex<T>>(N));
-    for (std::size_t j = 0; j < N; ++j) {
-      E[j] = temp_E[j];
-      for (std::size_t k = 0; k < N; ++k) {
-        // U[j] is an eigenvector of H with eigenvalue E[j]
-        U[j][k] = temp_H[j * N + k];
-      }
+  /// @overload
+  template <class InOutMat, class InOutVec>
+  int matrix_vector_solve(InOutMat& A, InOutVec& b) {
+    using T = range_value_t<InOutMat>;
+    int info;
+    if constexpr (is_fixed_size_array_v<remove_cvref_t<InOutMat>>) {
+      constexpr std::size_t N = kspc::dim(A);
+      static std::array<T, N> ipiv;
+      info = matrix_vector_solve(A, ipiv, b);
+    } else {
+      const std::size_t n = kspc::dim(A);
+      std::vector<T> ipiv(n);
+      info = matrix_vector_solve(A, ipiv, b);
     }
-
-    delete[] temp_H;
-    delete[] temp_E;
-    delete[] cwork;
-    delete[] rwork;
-    return std::make_pair(std::move(E), std::move(U));
+    return info;
   }
 
   /// @}
 } // namespace kspc
+
+// eigen solve
+namespace kspc {
+  /// @addtogroup linalg
+  /// @{
+
+  /// @cond
+  namespace detail {
+    extern "C" {
+      // double
+
+      // solve Ax = λx with a symmetric matrix A
+      void dsyev_(const char& jobz,
+                  const char& uplo,
+                  const std::size_t& n,
+                  double* A,
+                  const std::size_t& lda,
+                  double* w,
+                  double* work,
+                  const std::size_t& lwork,
+                  int& info);
+
+      // complex double
+
+      // solve Ax = λx with a hermitian matrix A
+      void zheev_(const char& jobz,
+                  const char& uplo,
+                  const std::size_t& n,
+                  std::complex<double>* A,
+                  const std::size_t& lda,
+                  double* w,
+                  std::complex<double>* work,
+                  const std::size_t& lwork,
+                  double* rwork,
+                  int& info);
+    }
+  }
+  /// @endcond
+
+  /// solve Ax = λx with a hermitian matrix A
+  template <class InOutMat, class OutVec, class Work, class RWork>
+  int hermitian_matrix_eigen_solve(InOutMat& A, OutVec& w, Work& work, RWork& rwork) {
+    using std::size, std::data;
+    const std::size_t n = kspc::dim(A);
+    // std::cout << n << std::endl;
+    assert(size(A) == n * n);
+    assert(size(w) == n);
+    assert(size(work) >= 2 * n - 1);
+    assert(size(rwork) == 3 * n - 2);
+
+    int info;
+    //      zheev_(jobz, uplo, n,      A , lda,      w ,      work ,     lwork ,      rwork , info)
+    detail::zheev_( 'V',  'U', n, data(A),   n, data(w), data(work), size(work), data(rwork), info);
+    return info;
+  }
+
+  /// solve Ax = λx with a symmetric matrix A
+  template <class InOutMat, class OutVec, class Work>
+  int symmetric_matrix_eigen_solve(InOutMat& A, OutVec& w, Work& work) {
+    using std::size, std::data;
+    const std::size_t n = kspc::dim(A);
+    // std::cout << n << std::endl;
+    assert(size(A) == n * n);
+    assert(size(w) == n);
+    assert(size(work) >= 3 * n - 1);
+    int info;
+    //      dsyev_(jobz, uplo, n,      A , lda,      w ,      work ,     lwork , info)
+    detail::dsyev_( 'V',  'U', n, data(A),   n, data(w), data(work), size(work), info);
+    return info;
+  }
+
+  /// @overload
+  template <class InOutMat, class OutVec>
+  int hermitian_matrix_eigen_solve(InOutMat& A, OutVec& w) {
+    using T = range_value_t<InOutMat>;
+    int info;
+    if constexpr (is_complex_v<range_value_t<InOutMat>>) {
+      if constexpr (is_fixed_size_array_v<remove_cvref_t<InOutMat>>) {
+        constexpr std::size_t N = kspc::dim(A);
+        static std::array<T, 4 * N> work;
+        static std::array<T, 3 * N - 2> rwork;
+        info = hermitian_matrix_eigen_solve(A, w, work, rwork);
+      } else {
+        const std::size_t n = kspc::dim(A);
+        std::vector<T> work(4 * n);
+        std::vector<T> rwork(3 * n - 2);
+        info = hermitian_matrix_eigen_solve(A, w, work, rwork);
+      }
+    } else {
+      if constexpr (is_fixed_size_array_v<remove_cvref_t<InOutMat>>) {
+        constexpr std::size_t N = kspc::dim(A);
+        static std::array<T, 6 * N> work;
+        info = symmetric_matrix_eigen_solve(A, w, work);
+      } else {
+        const std::size_t n = kspc::dim(A);
+        std::vector<T> work(6 * n);
+        info = symmetric_matrix_eigen_solve(A, w, work);
+      }
+    }
+    return info;
+  }
+
+  /// @}
+} // namespace kspc
+
+// clang-format on
