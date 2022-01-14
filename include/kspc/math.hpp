afb1a8c2737748f97444e85011230d9a3ee6045a
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-01-14 18:05:01 +0900

    Small fix for math constants

--- include/kspc/math.hpp
+++ include/kspc/math.hpp
@@ -21,29 +21,41 @@ namespace kspc {
   /// Imaginary unit
   template <typename T> inline constexpr std::enable_if_t<std::is_arithmetic_v<T>, std::complex<T>> i_v{0, 1};
   /// The Euler's number
-  template <typename T> inline constexpr T e_v          = detail::enable_if_floating<T>(2.718281828459045235360287471352662498L);
+  template <typename T> inline constexpr detail::enable_if_floating<T>
+  e_v          = T(2.718281828459045235360287471352662498L);
   /// log_2 e
-  template <typename T> inline constexpr T log2e_v      = detail::enable_if_floating<T>(1.442695040888963407359924681001892137L);
+  template <typename T> inline constexpr detail::enable_if_floating<T>
+  log2e_v      = T(1.442695040888963407359924681001892137L);
   /// log_10 e
-  template <typename T> inline constexpr T log10e_v     = detail::enable_if_floating<T>(0.434294481903251827651128918916605082L);
+  template <typename T> inline constexpr detail::enable_if_floating<T>
+  log10e_v     = T(0.434294481903251827651128918916605082L);
   /// log_e 2
-  template <typename T> inline constexpr T ln2_v        = detail::enable_if_floating<T>(0.693147180559945309417232121458176568L);
+  template <typename T> inline constexpr detail::enable_if_floating<T>
+  ln2_v        = T(0.693147180559945309417232121458176568L);
   /// log_e 10
-  template <typename T> inline constexpr T ln10_v       = detail::enable_if_floating<T>(2.302585092994045684017991454684364208L);
+  template <typename T> inline constexpr detail::enable_if_floating<T>
+  ln10_v       = T(2.302585092994045684017991454684364208L);
   /// pi
-  template <typename T> inline constexpr T pi_v         = detail::enable_if_floating<T>(3.141592653589793238462643383279502884L);
+  template <typename T> inline constexpr detail::enable_if_floating<T>
+  pi_v         = T(3.141592653589793238462643383279502884L);
   /// tau
-  template <typename T> inline constexpr T tau_v        = detail::enable_if_floating<T>(6.283185307179586476925286766559005768L);
+  template <typename T> inline constexpr detail::enable_if_floating<T>
+  tau_v        = T(6.283185307179586476925286766559005768L);
   /// sqrt 2
-  template <typename T> inline constexpr T sqrt2_v      = detail::enable_if_floating<T>(1.414213562373095048801688724209698079L);
+  template <typename T> inline constexpr detail::enable_if_floating<T>
+  sqrt2_v      = T(1.414213562373095048801688724209698079L);
   /// sqrt 3
-  template <typename T> inline constexpr T sqrt3_v      = detail::enable_if_floating<T>(1.732050807568877293527446341505872367L);
+  template <typename T> inline constexpr detail::enable_if_floating<T>
+  sqrt3_v      = T(1.732050807568877293527446341505872367L);
   /// sqrt 5
-  template <typename T> inline constexpr T sqrt5_v      = detail::enable_if_floating<T>(2.236067977499789696409173668731276235L);
+  template <typename T> inline constexpr detail::enable_if_floating<T>
+  sqrt5_v      = T(2.236067977499789696409173668731276235L);
   /// The Euler-Mascheroni constant
-  template <typename T> inline constexpr T egamma_v     = detail::enable_if_floating<T>(0.577215664901532860606512090082402431L);
+  template <typename T> inline constexpr detail::enable_if_floating<T>
+  egamma_v     = T(0.577215664901532860606512090082402431L);
   /// Exponential of the Euler-Mascheroni constant
-  template <typename T> inline constexpr T exp_egamma_v = detail::enable_if_floating<T>(1.781072417990197985236504103107179549L);
+  template <typename T> inline constexpr detail::enable_if_floating<T>
+  exp_egamma_v = T(1.781072417990197985236504103107179549L);
 
   /// alias for `i_v<double>`
   inline constexpr std::complex<double> i = i_v<double>;
