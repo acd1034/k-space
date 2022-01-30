a653a94368017cee1ae3b80f938fffe3d97d48a2
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-01-30 15:57:51 +0900

    eigen-solve-no-evec

--- include/kspc/linalg.hpp
+++ include/kspc/linalg.hpp
@@ -475,4 +475,90 @@ namespace kspc::hermitian {
   /// @}
 } // namespace kspc
 
+// hermitian matrix eigen solve without eigenvectors
+namespace kspc::hermitian {
+  /// @addtogroup linalg
+  /// @{
+
+  /// solve Ax = λx with a symmetric matrix A without eigenvectors
+  template <class InOutMat, class OutVec, class Work>
+  std::enable_if_t<std::conjunction_v<
+    is_range<InOutMat>, is_range<OutVec>, is_range<Work>>, int>
+  eigen_solve_no_evec(InOutMat& A, OutVec& w, Work& work) {
+    using std::size, std::data; // for ADL
+    const std::size_t n = kspc::dim(A);
+    // std::cout << n << std::endl;
+    assert(size(A) == n * n);
+    assert(size(w) == n);
+    assert(size(work) >= 3 * n - 1);
+
+    int info;
+    //      dsyev_(jobz, uplo, n,      A , lda,      w ,      work ,     lwork , info)
+    detail::dsyev_( 'N',  'U', n, data(A),   n, data(w), data(work), size(work), info);
+    return info;
+  }
+
+  /// solve Ax = λx with a hermitian matrix A without eigenvectors
+  template <class InOutMat, class OutVec, class Work, class RWork>
+  std::enable_if_t<std::conjunction_v<
+    is_range<InOutMat>, is_range<OutVec>, is_range<Work>, is_range<RWork>>, int>
+  eigen_solve_no_evec(InOutMat& A, OutVec& w, Work& work, RWork& rwork) {
+    using std::size, std::data; // for ADL
+    const std::size_t n = kspc::dim(A);
+    // std::cout << n << std::endl;
+    assert(size(A) == n * n);
+    assert(size(w) == n);
+    assert(size(work) >= 2 * n - 1);
+    assert(size(rwork) == 3 * n - 2);
+
+    int info;
+    //      zheev_(jobz, uplo, n,      A , lda,      w ,      work ,     lwork ,      rwork , info)
+    detail::zheev_( 'N',  'U', n, data(A),   n, data(w), data(work), size(work), data(rwork), info);
+    return info;
+  }
+
+  /// @overload
+  template <class InOutMat, class OutVec, class M, class P = identity_fn>
+  std::enable_if_t<
+    is_range_v<InOutMat> and is_range_v<OutVec> and (not is_range_v<M>) and (not is_range_v<P>), int>
+  eigen_solve_no_evec(InOutMat& A, OutVec& w, M&& map, P&& proj = {}) {
+    using T = remove_cvref_t<std::invoke_result_t<P&, range_reference_t<InOutMat>>>;
+    int info;
+
+    if constexpr (is_fixed_size_array_v<remove_cvref_t<InOutMat>>) {
+      constexpr std::size_t N = fixed_size_matrix_dim_v<remove_cvref_t<InOutMat>>;
+      static std::array<T, N * N> B;
+      constexpr auto column_major = mapping_column_major(N);
+      matrix_copy(A, B, map, column_major, proj);
+
+      if constexpr (is_complex_v<T>) {
+        static std::array<T, 4 * N> work;
+        static std::array<complex_value_t<T>, N == 0 ? 0 : 3 * N - 2> rwork;
+        info = eigen_solve_no_evec(B, w, work, rwork);
+      } else {
+        static std::array<T, 6 * N> work;
+        info = eigen_solve_no_evec(B, w, work);
+      }
+    } else {
+      const std::size_t n = kspc::dim(A);
+      std::vector<T> B(n * n);
+      const auto column_major = mapping_column_major(n);
+      matrix_copy(A, B, map, column_major, proj);
+
+      if constexpr (is_complex_v<T>) {
+        std::vector<T> work(4 * n);
+        std::vector<complex_value_t<T>> rwork(n == 0 ? 0 : 3 * n - 2);
+        info = eigen_solve_no_evec(B, w, work, rwork);
+      } else {
+        std::vector<T> work(6 * n);
+        info = eigen_solve_no_evec(B, w, work);
+      }
+    }
+
+    return info;
+  }
+
+  /// @}
+} // namespace kspc
+
 // clang-format on
