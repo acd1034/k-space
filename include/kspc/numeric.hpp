6538a72fd5d1a5f51ec6baefe6a198c078cf4e03
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-02-22 20:36:27 +0900

    Rename BOp→Op, bop→op

--- include/kspc/numeric.hpp
+++ include/kspc/numeric.hpp
@@ -17,29 +17,28 @@ namespace kspc {
 
   /// @cond
   namespace detail {
-    // NOTE: irrelevant to `std::projected`
     template <class P, class I>
     using projected_t = std::invoke_result_t<P&, iter_reference_t<I>>;
 
     template <class I, class T,
-              class BOp = std::plus<>,
+              class Op = std::plus<>,
               class P = identity_fn>
     struct sum_constraints : std::conjunction<
       std::is_invocable<P&, iter_reference_t<I>>,
-      std::is_invocable<BOp&, T, projected_t<P, I>>> {};
+      std::is_invocable<Op&, T, projected_t<P, I>>> {};
 
     template <class I, class S, class T,
-              class BOp = std::plus<>,
+              class Op = std::plus<>,
               class P = identity_fn,
               std::enable_if_t<std::conjunction_v<
                 is_sentinel_for<S, I>,
                 is_input_iterator<I>,
-                sum_constraints<I, T, BOp, P>>, std::nullptr_t> = nullptr>
-    constexpr auto sum(I first, S last, T init, BOp bop = {}, P proj = {}) {
-      using U = remove_cvref_t<std::invoke_result_t<BOp&, T, projected_t<P, I>>>;
+                sum_constraints<I, T, Op, P>>, std::nullptr_t> = nullptr>
+    constexpr auto sum(I first, S last, T init, Op op = {}, P proj = {}) {
+      using U = remove_cvref_t<std::invoke_result_t<Op&, T, projected_t<P, I>>>;
       U ret = std::move(init);
       for (; first != last; ++first) {
-        ret = std::invoke(bop, std::move(ret), std::invoke(proj, *first));
+        ret = std::invoke(op, std::move(ret), std::invoke(proj, *first));
       }
       return ret;
     }
@@ -47,18 +46,18 @@ namespace kspc {
   /// @endcond
 
   /// @brief `std::accumulate` without the initial value
-  /// @note The order of the arguments `P`, `BOp` is different from range-v3.
+  /// @note The order of the arguments `P`, `Op` is different from range-v3.
   template <class R,
             class P = identity_fn,
-            class BOp = std::plus<>,
+            class Op = std::plus<>,
             std::enable_if_t<is_range_v<R>, std::nullptr_t> = nullptr>
-  constexpr auto sum(R&& r, P proj = {}, BOp bop = {}) {
+  constexpr auto sum(R&& r, P proj = {}, Op op = {}) {
     using std::begin, std::end, std::empty; // for ADL
     assert(!empty(r));
 
     auto first = begin(r);
     auto init = std::invoke(proj, *first++);
-    return detail::sum(first, end(r), init, std::move(bop), std::move(proj));
+    return detail::sum(first, end(r), init, std::move(op), std::move(proj));
   }
 
   // innerp
@@ -66,22 +65,22 @@ namespace kspc {
   /// @cond
   namespace detail {
     template <class I1, class I2, class T,
-              class BOp1 = std::plus<>,
-              class BOp2 = std::multiplies<>,
+              class Op1 = std::plus<>,
+              class Op2 = std::multiplies<>,
               class P1 = conj_fn,
               class P2 = identity_fn>
     struct innerp_constraints : std::conjunction<
       std::is_invocable<P1&, iter_value_t<I1>>,
       std::is_invocable<P2&, iter_value_t<I2>>,
-      std::is_invocable<BOp2&, projected_t<P1, I1>, projected_t<P2, I2>>,
+      std::is_invocable<Op2&, projected_t<P1, I1>, projected_t<P2, I2>>,
       std::is_invocable<
-        BOp1&,
+        Op1&,
         T,
-        std::invoke_result_t<BOp2&, projected_t<P1, I1>, projected_t<P2, I2>>>> {};
+        std::invoke_result_t<Op2&, projected_t<P1, I1>, projected_t<P2, I2>>>> {};
 
     template <class I1, class S1, class I2, class S2, class T,
-              class BOp1 = std::plus<>,
-              class BOp2 = std::multiplies<>,
+              class Op1 = std::plus<>,
+              class Op2 = std::multiplies<>,
               class P1 = conj_fn,
               class P2 = identity_fn,
               std::enable_if_t<std::conjunction_v<
@@ -89,21 +88,21 @@ namespace kspc {
                 is_sentinel_for<S2, I2>,
                 is_input_iterator<I1>,
                 is_input_iterator<I2>,
-                innerp_constraints<I1, I2, T, BOp1, BOp2, P1, P2>>, std::nullptr_t> = nullptr>
+                innerp_constraints<I1, I2, T, Op1, Op2, P1, P2>>, std::nullptr_t> = nullptr>
     constexpr auto innerp(I1 first1, S1 last1, I2 first2, S2 last2, T init,
-                          BOp1 bop1 = {}, BOp2 bop2 = {}, P1 proj1 = {}, P2 proj2 = {}) {
+                          Op1 op1 = {}, Op2 op2 = {}, P1 proj1 = {}, P2 proj2 = {}) {
       using U = remove_cvref_t<
         std::invoke_result_t<
-          BOp1&,
+          Op1&,
           T,
-          std::invoke_result_t<BOp2&, projected_t<P1, I1>, projected_t<P2, I2>>>>;
+          std::invoke_result_t<Op2&, projected_t<P1, I1>, projected_t<P2, I2>>>>;
       U ret = std::move(init);
       for (; first1 != last1 && first2 != last2; ++first1, ++first2) {
         ret =
           std::invoke(
-            bop1,
+            op1,
             std::move(ret),
-            std::invoke(bop2, std::invoke(proj1, *first1), std::invoke(proj2, *first2)));
+            std::invoke(op2, std::invoke(proj1, *first1), std::invoke(proj2, *first2)));
       }
       return ret;
     }
@@ -111,18 +110,18 @@ namespace kspc {
   /// @endcond
 
   /// @brief `std::inner_product` without the initial value
-  /// @note The order of the arguments `P#`, `BOp#` is different from range-v3.
+  /// @note The order of the arguments `P#`, `Op#` is different from range-v3.
   template <class R1, class R2,
             class P1 = conj_fn,
             class P2 = identity_fn,
-            class BOp1 = std::plus<>,
-            class BOp2 = std::multiplies<>,
+            class Op1 = std::plus<>,
+            class Op2 = std::multiplies<>,
             std::enable_if_t<std::conjunction_v<
               is_range<R1>,
               is_range<R2>,
               std::negation<is_range<P1>>>, std::nullptr_t> = nullptr>
   constexpr auto innerp(R1&& r1, R2&& r2,
-                        P1 proj1 = {}, P2 proj2 = {}, BOp1 bop1 = {}, BOp2 bop2 = {}) {
+                        P1 proj1 = {}, P2 proj2 = {}, Op1 op1 = {}, Op2 op2 = {}) {
     using std::begin, std::end, std::empty; // for ADL
     assert(!empty(r1));
     assert(!empty(r2));
@@ -130,13 +129,13 @@ namespace kspc {
     auto first1 = begin(r1);
     auto first2 = begin(r2);
     auto init = std::invoke(
-                  bop2,
+                  op2,
                   std::invoke(proj1, *first1++),
                   std::invoke(proj2, *first2++));
     return detail::innerp(
              first1, end(r1), first2, end(r2), init,
-             std::move(bop1),
-             std::move(bop2),
+             std::move(op1),
+             std::move(op2),
              std::move(proj1),
              std::move(proj2));
   }
