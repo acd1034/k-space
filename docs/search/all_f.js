da2be4245f7f3205df7671748de3a0a56e1925ed
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-02-23 14:57:29 +0000

    Deploying to gh-pages from  @ a14aa1c25948b6dae99643923bc77ca3d97745a3 🚀

--- docs/search/all_f.js
+++ docs/search/all_f.js
@@ -2,8 +2,7 @@ var searchData=
 [
   ['tau_0',['tau',['../group__math__constants.html#ga2c933e2a32e66c2bad4f2189ff734f4e',1,'kspc']]],
   ['tau_5fv_1',['tau_v',['../group__math__constants.html#gabd2b63bce87d7b8248dcb5cff5cf78fe',1,'kspc']]],
-  ['to_5faddress_2',['to_address',['../group__range.html#gaff24f9800cb8865ba33697540f84664c',1,'kspc::to_address(T *p) noexcept'],['../group__range.html#gafeb1b0d13131e285bbbe164bcf4c4e37',1,'kspc::to_address(const P &amp;p) noexcept']]],
-  ['transpose_3',['transpose',['../structkspc_1_1mapping_1_1transpose.html',1,'kspc::mapping::transpose&lt; Mapping &gt;'],['../group__matrix.html#gaa93bed1866df29175a7e842735c6f3f5',1,'kspc::mapping::transpose(Mapping) -&gt; transpose&lt; Mapping &gt;']]],
-  ['type_5fidentity_4',['type_identity',['../structkspc_1_1type__identity.html',1,'kspc']]],
-  ['type_5fidentity_5ft_5',['type_identity_t',['../group__utility.html#gaded1ea76309ffc8cac73aad8d7455125',1,'kspc']]]
+  ['transpose_2',['transpose',['../structkspc_1_1mapping_1_1transpose.html',1,'kspc::mapping::transpose&lt; Mapping &gt;'],['../group__matrix.html#gaa93bed1866df29175a7e842735c6f3f5',1,'kspc::mapping::transpose(Mapping) -&gt; transpose&lt; Mapping &gt;']]],
+  ['type_5fidentity_3',['type_identity',['../structkspc_1_1type__identity.html',1,'kspc']]],
+  ['type_5fidentity_5ft_4',['type_identity_t',['../group__utility.html#gaded1ea76309ffc8cac73aad8d7455125',1,'kspc']]]
 ];
