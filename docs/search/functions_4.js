2ad85913b1ab25d8d7c87b48661e8b0613daae64
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-02-23 12:48:46 +0000

    Deploying to gh-pages from  @ e1179dc7d1fe51b83babe6517e03e611cd96a174 🚀

--- docs/search/functions_4.js
+++ docs/search/functions_4.js
@@ -3,5 +3,6 @@ var searchData=
   ['in_5fbrillouin_5fzone_0',['in_brillouin_zone',['../group__physics.html#ga6a923b5cab9583c56f42403b60382eef',1,'kspc']]],
   ['innerp_1',['innerp',['../group__numeric.html#ga2b3840c00748bc71e812b25c83683fdd',1,'kspc']]],
   ['integrate_2',['integrate',['../group__integration.html#gad395a406bc2f1b0af3f8d657e388ae41',1,'kspc::qag::integrate()'],['../group__integration.html#ga2e5db4e7f5c1094792b58850ebc4b5c9',1,'kspc::cquad::integrate()']]],
-  ['isqrt_3',['isqrt',['../group__matrix.html#ga43868c18ecfcdefd942b777ed870eb35',1,'kspc']]]
+  ['invoke_3',['invoke',['../group__utility.html#gaaf58d90fe3dcd12e6efb88b4dbd6366c',1,'kspc']]],
+  ['isqrt_4',['isqrt',['../group__matrix.html#ga43868c18ecfcdefd942b777ed870eb35',1,'kspc']]]
 ];
