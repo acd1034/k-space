cf9043bed21172f7d06f5a2aa48b0a167107a86f
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-02-23 21:38:36 +0900

    Add constexpr invoke

--- include/kspc/numeric.hpp
+++ include/kspc/numeric.hpp
@@ -6,6 +6,51 @@
 #include <vector>
 #include <kspc/core.hpp> // is_input_range, identity_fn, conj_fn
 
+// constexpr invoke (invoke is constexpr after C++20)
+// https://en.cppreference.com/w/cpp/utility/functional/invoke
+namespace kspc {
+  /// @addtogroup utility
+  /// @{
+
+  namespace detail {
+    template <class>
+    constexpr bool is_reference_wrapper_v = false;
+
+    template <class U>
+    constexpr bool is_reference_wrapper_v<std::reference_wrapper<U>> = true;
+
+    template <class C, class Pointed, class T1, class... Args>
+    constexpr decltype(auto) invoke_memptr(Pointed C::*f, T1&& t1, Args&&... args) {
+      if constexpr (std::is_function_v<Pointed>) {
+        if constexpr (std::is_base_of_v<C, std::decay_t<T1>>)
+          return (std::forward<T1>(t1).*f)(std::forward<Args>(args)...);
+        else if constexpr (is_reference_wrapper_v<std::decay_t<T1>>)
+          return (t1.get().*f)(std::forward<Args>(args)...);
+        else
+          return ((*std::forward<T1>(t1)).*f)(std::forward<Args>(args)...);
+      } else {
+        static_assert(std::is_object_v<Pointed> && sizeof...(args) == 0);
+        if constexpr (std::is_base_of_v<C, std::decay_t<T1>>) return std::forward<T1>(t1).*f;
+        else if constexpr (is_reference_wrapper_v<std::decay_t<T1>>)
+          return t1.get().*f;
+        else
+          return (*std::forward<T1>(t1)).*f;
+      }
+    }
+  } // namespace detail
+
+  template <class F, class... Args>
+  constexpr std::invoke_result_t<F, Args...>
+  invoke(F&& f, Args&&... args) noexcept(std::is_nothrow_invocable_v<F, Args...>) {
+    if constexpr (std::is_member_pointer_v<std::decay_t<F>>)
+      return detail::invoke_memptr(f, std::forward<Args>(args)...);
+    else
+      return std::forward<F>(f)(std::forward<Args>(args)...);
+  }
+
+  /// @}
+} // namespace kspc
+
 // clang-format off
 
 // Numerical algorithms
